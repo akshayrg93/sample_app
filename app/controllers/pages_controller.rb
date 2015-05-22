90a42939311b59623c5e0870c14ab0b20eb94ff5
akshayrg93 <akshay@qburst.com>
2015-05-22 15:03:15 +0530

    Done with static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,14 @@
 class PagesController < ApplicationController
   def home
 @title = "Home"
-end
-def contact
+	end
+	def contact
 @title = "Contact"
-end
+	end
 def about
 @title = "About"
+end
+def help
+@title = "Help"
 end
 end
